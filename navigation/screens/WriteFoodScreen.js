23f88153ea3109926f4a0c91c18fb99806a434b3
Nedim Zolj <74002374+zoljann@users.noreply.github.com>
2022-03-09 18:56:48 +0100

    styling writeFood

--- navigation/screens/WriteFoodScreen.js
+++ navigation/screens/WriteFoodScreen.js
@@ -2,22 +2,38 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.View`
-  padding: 15% 5% 0 5%;
-`;
-const ProductContainer = styled.View`
-  height: 85%;
+  margin: 10% 5% 10% 5%;
+  height: 90%;
+  background-color: gray;
 `;
 const Texts = styled.Text``;
-const MenuItems = styled.View`
+const AddButton = styled.TouchableOpacity`
+  margin: 0 auto 0 auto;
+  width: 60%;
+  height: 40px;
   align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.colors.primary};
+  border-radius: 50px;
 `;
-const Dugme = styled.TouchableOpacity`
-  background-color: red;
+const AddButtonText = styled.Text`
+  font-size: 20px;
+  color: ${(props) => props.theme.text.white};
+`;
+const TextInputStyled = styled.TextInput`
+  margin: 10% 0 10% 0;
+  font-size: 25px;
+  width: 100%;
+  text-align: center;
+  color: ${(props) => props.theme.text.white};
 `;
 const WriteFoodScreen = ({ navigation }) => {
   return (
     <Wrapper>
-      <Texts>xeex</Texts>
+      <TextInputStyled placeholder="Name" />
+      <AddButton>
+        <AddButtonText>Add food</AddButtonText>
+      </AddButton>
     </Wrapper>
   );
 };
