a31aeb70080762ac637fd6d14e2dc38511e7f3c0
Nedim Zolj <74002374+zoljann@users.noreply.github.com>
2022-03-11 01:38:00 +0100

    fix MenuIems, place on bottom

--- navigation/screens/ListsScreen.js
+++ navigation/screens/ListsScreen.js
@@ -9,37 +9,38 @@ import { TouchableOpacity } from "react-native";
 const Wrapper = styled.View`
   padding: 15% 2% 0 2%;
 `;
+const Container = styled.View`
+  height: 93%;
+`;
 const ProductContainer = styled.View``;
-const Texts = styled.Text``;
 const MenuItems = styled.View`
-  position: absolute;
   width: 100%;
   align-items: center;
-  justify-content: center;
-  text-align: center;
-  margin-top: 150%;
 `;
+
 const ListsScreen = ({ navigation }) => {
   return (
     <Wrapper>
-      <SearchBar
-        inputStyle={{ backgroundColor: "white" }}
-        containerStyle={{
-          backgroundColor: "white",
-          borderWidth: 1,
-          borderRadius: 40,
-          height: 42,
-          justifyContent: "center",
-          marginBottom: 10,
-        }}
-        placeholderTextColor={"#479FEC"}
-        placeholder={"Do i have.."}
-      />
-      <ProductContainer>
-        <Product foodName="Milk" foodExpireDate="13.2.2019" />
-        <Product foodName="Eggs" foodExpireDate="13.2.2019" />
-        <Product foodName="Pizza" foodExpireDate="13.2.2019" />
-      </ProductContainer>
+      <Container>
+        <SearchBar
+          inputStyle={{ backgroundColor: "white" }}
+          containerStyle={{
+            backgroundColor: "white",
+            borderWidth: 1,
+            borderRadius: 40,
+            height: 42,
+            justifyContent: "center",
+            marginBottom: 10,
+          }}
+          placeholderTextColor={"#479FEC"}
+          placeholder={"Do i have.."}
+        />
+        <ProductContainer>
+          <Product foodName="Milk" foodExpireDate="13.2.2019" />
+          <Product foodName="Eggs" foodExpireDate="13.2.2019" />
+          <Product foodName="Pizza" foodExpireDate="13.2.2019" />
+        </ProductContainer>
+      </Container>
       <MenuItems>
         <Menu />
       </MenuItems>
